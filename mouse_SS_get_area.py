5470e017c2b1b3888f9a9521d5d71b2eb6a9b9f1
dokidokitaiyou <118153441+dokidokitaiyou@users.noreply.github.com>
2023-01-23 21:38:37 +0900

    田中君の

---
+++ mouse_SS_get_area.py
@@ -0,0 +1,28 @@
+import pyautogui
+from pynput import mouse
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+
+def on_click(x, y, button, pressed):
+    screen_shot = pyautogui.screenshot()
+    screen_shot.save('ttttest.jpg')
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    if not pressed:
+        # Stop listener
+        return False
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format(
+        'down' if dy < 0 else 'up',
+        (x, y)))
+
+# Collect events until released
+with mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as listener:
+    listener.join()
